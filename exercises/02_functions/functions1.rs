c4fb7d5d96b5094ec8c05cc95a9073eff2c68efc
Todd Chen <todd.chen@marelli.com>
2024-06-14 19:06:23 +0800

    6.14

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,7 +3,10 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+fn call_me(){
+    println!("111111");
+}
 
 fn main() {
     call_me();
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number:u32) -> u32 {
+    let price;
+    if number > 40 {
+        price = number;
+    } else {
+        price = number * 2;
+    }
+    price
+}
 
 // Don't modify this function!
 #[test]
